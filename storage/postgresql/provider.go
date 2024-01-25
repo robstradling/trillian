fdd0eefc23a11064974b4a37d37786b611cb629f
Rob Stradling <rob@sectigo.com>
2024-01-25 15:40:35 +0000

    Convert from database/sql API to jackc/pgx/v5 API

--- storage/postgresql/provider.go
+++ storage/postgresql/provider.go
@@ -34,7 +34,7 @@ var (
 
 	postgresqlMu              sync.Mutex
 	postgresqlErr             error
-	postgresqlDB              *sql.DB
+	postgresqlDB              *pgxpool.Pool
 	postgresqlStorageInstance *postgresqlProvider
 )
 
@@ -42,7 +42,7 @@ var (
 //
 // TODO(pavelkalinnikov): Make the dependency of PostgreSQL quota provider from
 // PostgreSQL storage provider explicit.
-func GetDatabase() (*sql.DB, error) {
+func GetDatabase() (*pgxpool.Pool, error) {
 	postgresqlMu.Lock()
 	defer postgresqlMu.Unlock()
 	return getPostgreSQLDatabaseLocked()
@@ -55,7 +55,7 @@ func init() {
 }
 
 type postgresqlProvider struct {
-	db *sql.DB
+	db *pgxpool.Pool
 	mf monitoring.MetricFactory
 }
 
@@ -77,7 +77,7 @@ func newPostgreSQLStorageProvider(mf monitoring.MetricFactory) (storage.Provider
 
 // getPostgreSQLDatabaseLocked returns an instance of PostgreSQL database, or creates
 // one. Requires postgresqlMu to be locked.
-func getPostgreSQLDatabaseLocked() (*sql.DB, error) {
+func getPostgreSQLDatabaseLocked() (*pgxpool.Pool, error) {
 	if postgresqlDB != nil || postgresqlErr != nil {
 		return postgresqlDB, postgresqlErr
 	}
